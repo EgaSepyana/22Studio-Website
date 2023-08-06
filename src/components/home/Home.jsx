271e9d62cb8bd979837a51294d216882e48ceb46
EgaSepyana <egasepyana04@gmail.com>
2023-08-07 06:51:51 +0700

    feat: :fire: Initial Commits

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Button variant='contained'>Hello Word</Button>
+    </div>
+  )
+}
+
+export default Home
